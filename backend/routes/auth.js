08bee1930ba3f36b210fbd009fb663e418badd14
Umesh Sahani <umeshsahani1998@gmail.com>
2021-12-03 21:42:33 +0530

    Add express-validator and setup for unique valut

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+// import package from express-validator 
+const { body, validationResult } = require('express-validator');
 
+// Create a User using: POST "api/auth". Does't require login
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 5 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password Length must be minimum length 5').isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: "Please Enter Unique Value for email.", message: err.message })
+        });
 
-// Create a User using: POST "api/auth". Does't require Auth
-router.get('/', (req, res)=> {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-
-})
+});
 
 module.exports = router;
