a06fba62e6a94c883bf66580d34830f7a1feeefc
Umesh Sahani <umeshsahani1998@gmail.com>
2021-12-04 14:29:55 +0530

    bcryptjs & jsonwebtoken impoted and define method to token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,11 @@ const User = require('../models/User');
 const router = express.Router();
 // import package from express-validator 
 const { body, validationResult } = require('express-validator');
+// import bcryptjs for password hashing and jwtwebtoken
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'BalSavi Studio is best';
 
 // Create a User using: POST "api/auth/createUser" login no required
 router.post('/createUser', [
@@ -15,20 +20,29 @@ router.post('/createUser', [
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    // Check wheather the user this email exists already
     try {
-
+        // Check wheather the user this email exists already
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Alert!! a user with this email already exists" });
         }
-        user = await Userd.create({
+        // password hashing mechanism
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        // Create a new user
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
-        })
+            password: secPass
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
 
-        res.json(user);
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Some error occur');
