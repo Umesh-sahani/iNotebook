3323cb9132012c7c2c0766efd21bc4335891b40a
Umesh Sahani <umeshsahani1998@gmail.com>
2021-12-08 15:10:09 +0530

    update an existing notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -31,14 +31,14 @@ router.post('/addnote', fetchuser, [
 
     try {
         //access elements via array destructuring
-        const {title, description,tag} = req.body;  
+        const { title, description, tag } = req.body;
 
         const note = new Note({
             title, description, tag, user: req.user.id
         });
 
         const savedNote = await note.save();
-        res.json({savedNote});
+        res.json({ savedNote });
 
     } catch (error) {
         console.log(error.message);
@@ -47,4 +47,42 @@ router.post('/addnote', fetchuser, [
 
 });
 
+
+//Route: 2 update an existing note using: POST "api/notes/addnote" | login required
+router.put('/updatenote/:id', fetchuser, [
+    body('title', 'Enter New title').isLength({ min: 5 }),
+    body('description', 'Enter new description length should be 20 char').isLength({ min: 20 })
+], async (req, res) => {
+    //If any error ocur, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        // access elements
+        const {title, description, tag} = req.body;
+        const newNote = {};
+
+        if(title){newNote.title = title}
+        if(description){newNote.description = description}
+        if(tag){newNote.tag = tag}
+
+        // Find the note to be updated add update it
+        let note = await Note.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not Found");
+        }
+        // Check the user are authorised or not
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+        res.json({note});
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
 module.exports = router;
